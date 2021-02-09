26dc63c32475eb7be26ca800914e811451ca5af4
Zakaria <zakPro@live.be>
2021-02-09 22:17:47 +0100

    fonctionnalte-titre_fixé-js-fini

--- public/js/main.js
+++ public/js/main.js
@@ -27,17 +27,40 @@ let connect = document.querySelector("#connexion")
 let menuCo = document.querySelector(".menuCo")
 
 connect.addEventListener('click', () => {
-    menuCo.className= ("menuCo dropdown-menu col-12 text-center d-none", "menuCo dropdown-menu col-12 text-center d-block")
-    if (menuCo.className == "menuCo dropdown-menu col-12 text-center d-block") {
+    menuCo.className= ("menuCo dropdown-menu col-12 text-center  px-17 d-none", "menuCo dropdown-menu col-12 text-center  px-17 d-block")
+    if (menuCo.className == "menuCo dropdown-menu col-12 text-center  px-17 d-block") {
         connect.addEventListener('click', () => {
-            menuCo.className= ("menuCo dropdown-menu col-12 text-center d-block", "menuCo dropdown-menu col-12 text-center d-none")
-            if (menuCo.className == "menuCo dropdown-menu col-12 text-center d-none") {
+            menuCo.className= ("menuCo dropdown-menu col-12 text-center  px-17 d-block", "menuCo dropdown-menu col-12 text-center  px-17 d-none")
+            if (menuCo.className == "menuCo dropdown-menu col-12 text-center  px-17 d-none") {
                 connect.addEventListener('click', () => {
-                    menuCo.className= ("menuCo dropdown-menu col-12 text-center d-none", "menuCo dropdown-menu col-12 text-center d-block")
+                    menuCo.className= ("menuCo dropdown-menu col-12 text-center  px-17 d-none", "menuCo dropdown-menu col-12 text-center  px-17 d-block")
                 })
             }
         })
     }
 })
+
+// FONCTIONNALITE BUTTON SCROLL QUI CHANGE LA PLACE DU TITRE 
+let hCacher = document.querySelector(".hcacher")
+let hPrincip = document.querySelector(".hprinc")
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        hCacher.className= ("hcacher d-none", "hcacher d-block")
+    } else if (window.scrollY < 100){
+        hCacher.className= ("hcacher d-block", "hcacher d-none")
+    }
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        hPrincip.className= ("hprinc d-block", "hprinc d-none")
+    } else if (window.scrollY < 100) {
+        hPrincip.className= ("hprinc d-none", "hprinc d-block")
+    }
+})
+
+
+// FONCTIONNALITE CAROUSEL JS 
 
 
