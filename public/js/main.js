31dd68b7592f90b44dc0b57007e678da73eaaf82
Zakaria <zakPro@live.be>
2021-02-09 16:42:30 +0100

    fonctionnaliter_js_button_noir&blanc-fini

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,18 @@ let para = document.querySelectorAll("p")
 console.log(buttonNoir);
 buttonNoir.addEventListener('click', () => {
     document.body.style.backgroundColor= "black"
+    // para.style.color="white"
+    let para2 = Array.from(para)
+    para2.forEach(el => {
+        el.style.color= "white"
+});
 })
 
 buttonBlanc.addEventListener('click', () => {
     document.body.style.backgroundColor= "white"
+    let para2 = Array.from(para)
+    para2.forEach(el => {
+        el.style.color= "black"
+});
 })
+
